bba8aad3433e65d942977e5c8a1a2634207441d3
Prashanth Selvam <prashanth.selvam@gmail.com>
2024-07-13 16:25:28 -0400

    Revert  map style

--- app/components/Map.tsx
+++ app/components/Map.tsx
@@ -7,12 +7,12 @@ import Map, { NavigationControl } from 'react-map-gl/maplibre';
 const TriggerCitiesMap = () => {
   const mapRef = React.useRef(null);
 
-  React.useEffect(() => {
-    if (mapRef.current) {
-      console.log(mapRef.current);
-      window.global.map = mapRef.current;
-    }
-  }, [mapRef.current]);
+  //   React.useEffect(() => {
+  //     if (mapRef.current) {
+  //       console.log(mapRef.current);
+  //       window.global.map = mapRef.current;
+  //     }
+  //   }, [mapRef.current]);
 
   return (
     <>
@@ -20,7 +20,7 @@ const TriggerCitiesMap = () => {
         <link href="https://unpkg.com/maplibre-gl@4.5.0/dist/maplibre-gl.css" rel="stylesheet" />
       </Head>
       <Map
-        onDrag={() => (window.map = mapRef.current)}
+        // onDrag={() => (window.map = mapRef.current)}
         ref={mapRef}
         initialViewState={{
           longitude: -122.4,
