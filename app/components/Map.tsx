7f2c24a1b1788ee3f22a9037c66a2aee7a167f6c
Prashanth Selvam <prashanth.selvam@gmail.com>
2024-07-13 16:10:46 -0400

    Update map style

--- app/components/Map.tsx
+++ app/components/Map.tsx
@@ -5,19 +5,30 @@ import Head from 'next/head';
 import Map from 'react-map-gl/maplibre';
 
 const TriggerCitiesMap = () => {
+  const mapRef = React.useRef(null);
+
+  React.useEffect(() => {
+    if (mapRef.current) {
+      console.log(mapRef.current);
+      window.global.map = mapRef.current;
+    }
+  }, [mapRef.current]);
+
   return (
     <>
       <Head>
         <link href="https://unpkg.com/maplibre-gl@4.5.0/dist/maplibre-gl.css" rel="stylesheet" />
       </Head>
       <Map
+        onDrag={() => (window.map = mapRef.current)}
+        ref={mapRef}
         initialViewState={{
           longitude: -122.4,
           latitude: 37.8,
           zoom: 2,
         }}
         style={{ width: '100%', height: 720 }}
-        mapStyle="https://raw.githubusercontent.com/openmaptiles/dark-matter-gl-style/master/style.json"
+        mapStyle="https://raw.githubusercontent.com/datacult/chartmetric-trigger-cities/main/map_style.json"
         scrollZoom={false}
       />
     </>
