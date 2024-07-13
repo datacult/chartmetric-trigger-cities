fc77b8e0cca99eec84b81d6b436bde4510272b5d
Prashanth Selvam <prashanth.selvam@gmail.com>
2024-07-13 11:41:51 -0400

    Initial commit

---
+++ app/content.ts
@@ -0,0 +1,13 @@
+export const YEAR = '2024';
+
+export const BODY = `
+    Lorem ipsum dolor sit amet consectetur. Eget suscipit purus est mauris. Magna odio tempor diam scelerisque. Tincidunt eu sed pretium accumsan commodo hendrerit. Risus eu enim volutpat in risus magna.
+    
+    Vulputate a eu quis dignissim vulputate in nunc lectus. Risus urna sagittis lectus dis aliquet cursus. Rutrum elementum integer sollicitudin orci pulvinar aliquet. Tristique leo massa dui amet aenean viverra sit. Mi nulla turpis pellentesque at elit pellentesque. Vulputate iaculis lobortis aenean laoreet vel maecenas eu non blandit. Massa vestibulum mattis eu diam.
+
+    Faucibus mauris lorem pellentesque egestas fusce sed viverra pharetra facilisis. Cursus ut ipsum velit sem. Cras est nulla sed ipsum convallis magna nunc quam. Nunc cursus ante fringilla dolor elit tincidunt scelerisque. Auctor in cursus urna non egestas. Lacus consectetur at quam nascetur scelerisque netus. Tempus nullam et id nulla ultrices vitae varius auctor aliquam. Posuere ullamcorper mauris ullamcorper nisl cursus tortor. Etiam lobortis adipiscing tincidunt nibh velit dignissim aliquam. Tortor suspendisse scelerisque arcu neque adipiscing.
+
+    Risus dignissim ut morbi sapien nec tincidunt orci. At dictum malesuada ut at purus tempus sit. At sit pulvinar risus pharetra facilisis ac sagittis. Suscipit odio sed elementum mauris in et praesent. Nibh mauris porta vehicula nec turpis id cras iaculis. Condimentum at augue purus varius risus tortor. Ullamcorper senectus id sit sit malesuada quis. Tincidunt sit habitant id eros bibendum ultrices. Sit nibh quis feugiat augue sapien nulla sapien.
+
+    Molestie pretium mauris nibh amet sed purus. Condimentum nunc enim facilisis mauris urna tortor in. Rhoncus lectus consectetur egestas augue lacus ornare tristique diam. Vestibulum sit lacinia ipsum elit nam bibendum amet in ligula. Facilisi lacus volutpat lorem fusce. Rhoncus tortor lorem morbi dui etiam.
+`;
